c7e4119e862bff0399f26477a08a9bd5cc3496fa
DheerajS-Dev <dheerajs.code@gmail.com>
2024-12-23 21:06:40 +0530

    Application Initialization

---
+++ src/main/java/code/dheeraj/TimeLiner/TimeLiner.java
@@ -0,0 +1,15 @@
+package code.dheeraj.TimeLiner;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TimeLiner {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TimeLiner.class, args);
+    }
+
+
+}
